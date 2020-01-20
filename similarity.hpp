084d6ace29efe22a75fca43b7b4a3363e40b0e8f
oozzy <oozzy.tlo@gmail.com>
2020-01-19 19:49:58 -0800

    Add missing include guard

--- similarity.hpp
+++ similarity.hpp
@@ -1,3 +1,6 @@
+#ifndef TLOFS_SIMILARITY_HPP
+#define TLOFS_SIMILARITY_HPP
+
 #include <cstdint>
 #include <filesystem>
 #include <utility>
@@ -13,3 +16,5 @@ double checkFileSimilarity(const std::filesystem::path &path1,
 double checkFileSimilarity(const std::filesystem::path &path1,
                            const std::filesystem::path &path2,
                            std::size_t blockSize, size_t numThreads);
+
+#endif  // TLOFS_SIMILARITY_HPP
