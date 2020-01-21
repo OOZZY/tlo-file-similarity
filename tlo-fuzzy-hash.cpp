c438b42ea6d68718b602fa2a4a1ae5d818b0ac9a
oozzy <oozzy.tlo@gmail.com>
2020-01-21 09:43:18 -0800

    Extract block of code to a new function

--- tlo-fuzzy-hash.cpp
+++ tlo-fuzzy-hash.cpp
@@ -7,6 +7,25 @@
 
 namespace fs = std::filesystem;
 
+void hashFiles(const std::vector<std::string> &arguments) {
+  for (std::size_t i = 0; i < arguments.size(); ++i) {
+    fs::path path = arguments[i];
+
+    if (fs::is_regular_file(path)) {
+      std::cout << fuzzyHash(path) << std::endl;
+    } else if (fs::is_directory(path)) {
+      for (auto &entry : fs::recursive_directory_iterator(path)) {
+        if (fs::is_regular_file(entry.path())) {
+          std::cout << fuzzyHash(entry.path()) << std::endl;
+        }
+      }
+    } else {
+      std::cerr << "Error: \"" << path.generic_string()
+                << "\" is not a file or directory." << std::endl;
+    }
+  }
+}
+
 int main(int argc, char **argv) {
   try {
     const CommandLineArguments arguments(argc, argv);
@@ -16,22 +35,7 @@ int main(int argc, char **argv) {
       return 1;
     }
 
-    for (std::size_t i = 0; i < arguments.arguments.size(); ++i) {
-      fs::path path = arguments.arguments[i];
-
-      if (fs::is_regular_file(path)) {
-        std::cout << fuzzyHash(path) << std::endl;
-      } else if (fs::is_directory(path)) {
-        for (auto &entry : fs::recursive_directory_iterator(path)) {
-          if (fs::is_regular_file(entry.path())) {
-            std::cout << fuzzyHash(entry.path()) << std::endl;
-          }
-        }
-      } else {
-        std::cerr << "Error: \"" << path.generic_string()
-                  << "\" is not a file or directory." << std::endl;
-      }
-    }
+    hashFiles(arguments.arguments);
   } catch (const std::exception &exception) {
     std::cerr << exception.what() << std::endl;
     return 1;
