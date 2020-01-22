4d91b65e9d41352d5c78b9998ee3a698059e7b94
oozzy <oozzy.tlo@gmail.com>
2020-01-21 23:56:54 -0800

    Allow tlo-find-similar-hashes to use more threads
    The program will still use only one thread by default.

--- tlo-find-similar-hashes.cpp
+++ tlo-find-similar-hashes.cpp
@@ -1,7 +1,10 @@
 #include <exception>
 #include <filesystem>
 #include <fstream>
+#include <functional>
 #include <iostream>
+#include <mutex>
+#include <thread>
 #include <vector>
 
 #include "fuzzy.hpp"
@@ -24,8 +27,6 @@ void readHashesFromFile(std::vector<FuzzyHash> &hashes, const fs::path &path) {
 
 void readHashes(std::vector<FuzzyHash> &hashes,
                 const std::vector<std::string> &arguments) {
-  std::cout << "Reading hashes." << std::endl;
-
   for (std::size_t i = 0; i < arguments.size(); ++i) {
     fs::path path = arguments[i];
 
@@ -39,9 +40,8 @@ void readHashes(std::vector<FuzzyHash> &hashes,
   }
 }
 
-void compareHashes(std::vector<FuzzyHash> &hashes, int similarityThreshold) {
-  std::cout << "Comparing hashes." << std::endl;
-
+void compareHashes(const std::vector<FuzzyHash> &hashes,
+                   int similarityThreshold) {
   for (std::size_t i = 0; i < hashes.size(); ++i) {
     for (std::size_t j = i + 1; j < hashes.size(); ++j) {
       if (hashesAreComparable(hashes[i], hashes[j])) {
@@ -57,16 +57,88 @@ void compareHashes(std::vector<FuzzyHash> &hashes, int similarityThreshold) {
   }
 }
 
+struct SharedState {
+  std::mutex indexMutex;
+  std::size_t index = 0;
+
+  std::mutex coutMutex;
+
+  const std::vector<FuzzyHash> &hashes;
+  const int similarityThreshold;
+
+  SharedState(const std::vector<FuzzyHash> &hashes_, int similarityThreshold_)
+      : hashes(hashes_), similarityThreshold(similarityThreshold_) {}
+};
+
+void compareHashAtIndexWithAllSubsequentHashes(SharedState &state) {
+  for (;;) {
+    std::unique_lock<std::mutex> indexUniqueLock(state.indexMutex);
+
+    if (state.index >= state.hashes.size()) {
+      break;
+    }
+
+    std::size_t i = state.index;
+    state.index++;
+    indexUniqueLock.unlock();
+
+    for (std::size_t j = i + 1; j < state.hashes.size(); ++j) {
+      if (hashesAreComparable(state.hashes[i], state.hashes[j])) {
+        double similarityScore =
+            compareHashes(state.hashes[i], state.hashes[j]);
+
+        if (similarityScore >= state.similarityThreshold) {
+          const std::lock_guard<std::mutex> coutLockGuard(state.coutMutex);
+          std::cout << '"' << state.hashes[i].path << "\" and \""
+                    << state.hashes[j].path << "\" are about "
+                    << similarityScore << "% similar." << std::endl;
+        }
+      }
+    }
+  }
+}
+
+// numThreads includes main thread.
+void compareHashes(const std::vector<FuzzyHash> &hashes,
+                   int similarityThreshold, std::size_t numThreads) {
+  if (numThreads <= 1) {
+    compareHashes(hashes, similarityThreshold);
+    return;
+  }
+
+  numThreads--;
+
+  SharedState state(hashes, similarityThreshold);
+  std::vector<std::thread> threads(numThreads);
+
+  for (auto &thread : threads) {
+    thread =
+        std::thread(compareHashAtIndexWithAllSubsequentHashes, std::ref(state));
+  }
+
+  compareHashAtIndexWithAllSubsequentHashes(state);
+
+  for (auto &thread : threads) {
+    thread.join();
+  }
+}
+
 constexpr int DEFAULT_SIMILARITY_THRESHOLD = 50;
 constexpr int MIN_SIMILARITY_THRESHOLD = 0;
 constexpr int MAX_SIMILARITY_THRESHOLD = 99;
+
+constexpr std::size_t DEFAULT_NUM_THREADS = 1;
+constexpr std::size_t MIN_NUM_THREADS = 1;
 
 const std::unordered_map<std::string, OptionAttributes> validOptions{
     {"--similarity-threshold",
      {true,
       "Display only the file pairs with a similarity score greater than or "
       "equal to this threshold (default: " +
-          std::to_string(DEFAULT_SIMILARITY_THRESHOLD) + ")."}}};
+          std::to_string(DEFAULT_SIMILARITY_THRESHOLD) + ")."}},
+    {"--num-threads",
+     {true, "Number of threads the program will use (default: " +
+                std::to_string(DEFAULT_NUM_THREADS) + ")."}}};
 
 int main(int argc, char **argv) {
   try {
@@ -79,6 +151,7 @@ int main(int argc, char **argv) {
     }
 
     int similarityThreshold = DEFAULT_SIMILARITY_THRESHOLD;
+    std::size_t numThreads = DEFAULT_NUM_THREADS;
 
     if (arguments.options.find("--similarity-threshold") !=
         arguments.options.end()) {
@@ -87,10 +160,18 @@ int main(int argc, char **argv) {
           MAX_SIMILARITY_THRESHOLD);
     }
 
+    if (arguments.options.find("--num-threads") != arguments.options.end()) {
+      numThreads =
+          arguments.getOptionValueAsULong("--num-threads", MIN_NUM_THREADS);
+    }
+
     std::vector<FuzzyHash> hashes;
 
+    std::cout << "Reading hashes." << std::endl;
     readHashes(hashes, arguments.arguments);
-    compareHashes(hashes, similarityThreshold);
+
+    std::cout << "Comparing hashes." << std::endl;
+    compareHashes(hashes, similarityThreshold, numThreads);
   } catch (const std::exception &exception) {
     std::cerr << exception.what() << std::endl;
     return 1;
